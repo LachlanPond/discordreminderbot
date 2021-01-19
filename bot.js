2e78d75ad143ec96184fbd8fb94093bf262e9fe8
Lachlan Pond <lachipond@gmail.com>
2021-01-20 00:40:49 +1000

    Help message text file and edge case fixes

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const Database = require('./classes');
+const fs = require('fs');
 
 if (process.env.NODE_ENV != 'production') {
     console.log('Running in Development mode');
@@ -16,25 +17,83 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+
     if (msg.content.startsWith('!reminders')) {
         console.log(`Command from ${msg.author.username}: !reminders`);
+
         let reminders = database.getReminderList(msg.author.id, msg.author.username);
         if (!reminders.length) {
-            msg.reply("You don't have any reminders. Use !remindme [reminder message] to set one.");
+            msg.reply("```You don't have any reminders. Use !remindme [reminder message] to set one.```");
             return;
         }
+
+        // Construct the message to be sent back to the user
         let replyMessage = "```";
         reminders.forEach((reminder, index) => {
             replyMessage += `${index}. ${reminder.message}\n`;
         });
         replyMessage += "```";
         msg.reply(replyMessage);
+
         console.log(`Replied to ${msg.author.username}'s !reminders command`);
     }
+
     if (msg.content.startsWith('!remindme')) {
         console.log(`Command from ${msg.author.username}: !remindme`);
+
+        // Get the reminder message by seperating out the command
         let reminder = msg.content.slice(msg.content.indexOf(' ')+1);
         database.addReminder(msg.author.id, msg.author.username, reminder);
+        msg.reply("```" + `Reminder added: ${reminder}` + "```");
+
         console.log(`Added reminder for ${msg.author.username}: ${msg.content.slice(msg.content.indexOf(' ')+1)}`);
     }
+
+    if (msg.content.startsWith('!reminderdelete')) {
+        console.log(`Command from ${msg.author.username}: !reminderdelete`);
+
+        try {
+            if (msg.content.split(' ').length === 1) {
+                msg.reply(`Please specify the number of the reminder you wish to delete`);
+                return;
+            }
+            let reminderNumber = msg.content.split(' ')[1];
+            let reminder = database.deleteReminder(msg.author.id, reminderNumber);
+            msg.reply("```" + `Reminder deleted: ${reminder}` + "```");
+            console.log(`Deleted reminder for ${msg.author.username}: ${reminder}`);
+        }
+        catch (err) {
+            if (err.message == 'No reminders') {
+                msg.reply("```You have no reminders to delete```");
+                return;
+            }
+            if (err.message == 'Reminder number is not an integer') {
+                msg.reply("```The reminder number must be an integer```");
+                return;
+            }
+            if (err.message == 'Reminder number out of bounds') {
+                msg.reply("```" + `The reminder number you gave is out of bounds. Expected values are from 0-${database.getReminderList(msg.author.id, msg.author.username).length-1}` + "```");
+                return;
+            }
+        }
+    }
+
+    if (msg.content.startsWith('!help')) {
+        console.log(`Command from ${msg.author.username}: !help`);
+
+        msg.reply("```" + getHelpMessage() + "```");
+
+        console.log(`Replied to ${msg.author.username}'s !help command`);
+    }
+    
 });
+
+function getHelpMessage() {
+    try {
+        return fs.readFileSync(process.env.HELP_FILE);
+    }
+    catch (err) {
+        console.log(`Reading help file failed: ${err.message}`);
+        return "Could not read help message file";
+    }
+}
--- classes.js
+++ classes.js
@@ -6,12 +6,16 @@ module.exports = class Database {
         this.file = file;
         this.loadFromFile()
     }
+
+    // Load the database from the database file given in the constructor
     loadFromFile() {
+        // Try to make the database file if it does not exist
         try {
             fs.writeFileSync(this.file, '{}', {flag: 'wx'})
             this.database = JSON.parse(fs.readFileSync(this.file));
         }
         catch (err) {
+            // If it does exist, fs.writeFileSync will throw an error with code EEXIST
             if (err.code === 'EEXIST') {
                 try {
                     this.database = JSON.parse(fs.readFileSync(this.file));
@@ -27,15 +31,18 @@ module.exports = class Database {
             }
         }
     }
+
     writeToFile() {
         fs.writeFileSync(this.file, JSON.stringify(this.database, null, 4));
     }
+
     getReminderList(userID, userName) {
         if (!this.database.hasOwnProperty(userID)) {
             this.addUser(userID, userName);
         }
         return this.database[userID].reminders;
     }
+
     addReminder(userID, userName, reminder) {
         if (!this.database.hasOwnProperty(userID)) {
             this.addUser(userID, userName);
@@ -48,6 +55,7 @@ module.exports = class Database {
         );
         this.writeToFile();
     }
+
     addUser(userID, userName) {
         this.database[userID] = {
             "name": userName,
@@ -55,4 +63,40 @@ module.exports = class Database {
         }
         this.writeToFile();
     }
+
+    deleteUser(userID) {
+        delete this.database[userID];
+        this.writeToFile();
+    }
+
+    deleteReminder(userID, reminderNumber) {
+        let reminders = this.database[userID].reminders;
+        if (reminders.length === 0) {
+            console.log(`There are no reminders to delete`);
+            throw new Error('No reminders');
+        }
+        if (!isInt(reminderNumber)) {
+            console.log(`Reminder number is not an integer: ${reminderNumber}`);
+            throw new Error('Reminder number is not an integer');
+        }
+        if (reminderNumber < 0 || reminderNumber > reminders.length-1) {
+            console.log(`Reminder number out of range (0-${reminders.length-1}): ${reminderNumber}`);
+            throw new Error('Reminder number out of bounds');
+        }
+        reminderNumber = parseInt(reminderNumber);
+        let reminderMessage = this.database[userID].reminders[reminderNumber].message;
+
+        this.database[userID].reminders.splice(reminderNumber, 1);
+        this.writeToFile();
+        return reminderMessage;
+    }
+}
+
+function isInt(value) {
+    var x;
+    if (isNaN(value)) {
+        return false;
+    }
+    x = parseFloat(value);
+    return (x | 0) === x;
 }
